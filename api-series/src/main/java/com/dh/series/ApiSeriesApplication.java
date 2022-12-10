048c859ba99b5a4bed4d1808f0d3ac273d824908
AlejaMarin <maialemarin@gmail.com>
2022-12-10 00:40:48 -0500

    Ajustes

--- api-series/src/main/java/com/dh/series/ApiSeriesApplication.java
+++ api-series/src/main/java/com/dh/series/ApiSeriesApplication.java
@@ -28,6 +28,9 @@ public class ApiSeriesApplication {
     public CommandLineRunner loadData(SerieRepository serieRepository) {
 
         UUID uuid = UUID.randomUUID();
+        UUID uuid1 = UUID.randomUUID();
+        UUID uuid2 = UUID.randomUUID();
+        UUID uuid3 = UUID.randomUUID();
         Chapter chapter = new Chapter(null, "Capitulo 1", 1, "www.netflix.com");
         List<Chapter> capitulos = new ArrayList<>();
         capitulos.add(chapter);
@@ -41,9 +44,9 @@ public class ApiSeriesApplication {
             }
 
             serieRepository.save(new Serie(uuid.toString(), "Serie 1", "Terror", temporadas));
-            serieRepository.save(new Serie(uuid.toString(), "Serie 2", "Terror", temporadas));
-            serieRepository.save(new Serie(uuid.toString(), "Serie 3", "Comedia", temporadas));
-            serieRepository.save(new Serie(uuid.toString(), "Serie 4", "Ficcion", temporadas));
+            serieRepository.save(new Serie(uuid1.toString(), "Serie 2", "Terror", temporadas));
+            serieRepository.save(new Serie(uuid2.toString(), "Serie 3", "Comedia", temporadas));
+            serieRepository.save(new Serie(uuid3.toString(), "Serie 4", "Ficcion", temporadas));
         };
     }
 
--- api-series/src/main/java/com/dh/series/service/SerieService.java
+++ api-series/src/main/java/com/dh/series/service/SerieService.java
@@ -5,7 +5,6 @@ import com.dh.series.repository.SerieRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class SerieService {
