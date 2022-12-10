38eba487c9ed948441c43b0f0b221a9f324ff3ab
AlejaMarin <maialemarin@gmail.com>
2022-12-10 00:15:13 -0500

    Dependencia de Eureka Client

--- api-series/src/main/java/com/dh/series/ApiSeriesApplication.java
+++ api-series/src/main/java/com/dh/series/ApiSeriesApplication.java
@@ -1,13 +1,50 @@
 package com.dh.series;
 
+import com.dh.series.model.Chapter;
+import com.dh.series.model.Season;
+import com.dh.series.model.Serie;
+import com.dh.series.repository.SerieRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@EnableMongoRepositories
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiSeriesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiSeriesApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner loadData(SerieRepository serieRepository) {
+
+        UUID uuid = UUID.randomUUID();
+        Chapter chapter = new Chapter(null, "Capitulo 1", 1, "www.netflix.com");
+        List<Chapter> capitulos = new ArrayList<>();
+        capitulos.add(chapter);
+        Season season = new Season(null, 1, capitulos);
+        List<Season> temporadas = new ArrayList<>();
+        temporadas.add(season);
+
+        return (args) -> {
+            if (!serieRepository.findAll().isEmpty()) {
+                return;
+            }
+
+            serieRepository.save(new Serie(uuid.toString(), "Serie 1", "Terror", temporadas));
+            serieRepository.save(new Serie(uuid.toString(), "Serie 2", "Terror", temporadas));
+            serieRepository.save(new Serie(uuid.toString(), "Serie 3", "Comedia", temporadas));
+            serieRepository.save(new Serie(uuid.toString(), "Serie 4", "Ficcion", temporadas));
+        };
     }
 
 }
